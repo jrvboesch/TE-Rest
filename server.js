87a45f55abf15e8149a6624b9df977401fce3a2a
juan <jrvboesch@gmail.com>
2016-09-21 16:42:32 -0600

    cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
+app.use(cors());
 app.locals.root_path = __dirname;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
